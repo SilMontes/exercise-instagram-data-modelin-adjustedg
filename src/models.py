050d955874a368f61c3902c1882f8d85a910ebd8
SilMontes <Silmontes2001@gmail.com>
2021-03-30 01:36:00 +0000

    Done

--- src/models.py
+++ src/models.py
@@ -8,23 +8,46 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, primary_key=True)
+    username = Column(String(80), nullable=False, unique=True)
+    firstname = Column(String(80), nullable=False)
+    lastname = Column(String(80), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    profile_url = Column(String(250), nullable=False)
+    created_at = Column(String(250))
+    updated_at = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    post_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    type_post = Column(String(250))
+    comment = Column(String(250))
+    ubication = Column(String(250))
+    created_at = Column(String(250))
+    updated_at = Column(String(250))
+    total_likes = Column(Integer)
+
+class PostComment(Base):
+    __tablename__ = 'postComment'
+    comment_id = Column(Integer, primary_key=True)
+    author_comment_id = Column(Integer, ForeignKey('user.user_id'))
+    comment_text = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.post_id'))
+    
+
+class Likes(Base):
+    __tablename__ = 'likes'
+    like_id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.post_id'))
+    user_likeit_id = Column(Integer, ForeignKey('user.user_id'))
+    time = Column(String(250))
 
     def to_dict(self):
         return {}
